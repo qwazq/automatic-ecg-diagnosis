f5c98c6f7dba5d7860e263cbefea3a4d24d5945f
LF-308 <hujingtian@126.com>
2024-01-22 03:33:08 +0800

    24-1-22:接下来np修改为元组版本

--- my_predict_graphPlot/graphPlot.py
+++ my_predict_graphPlot/graphPlot.py
@@ -12,8 +12,13 @@ gold_standard_path="gold_standard.csv"
 gold_standard_np=csvToNp(gold_standard_path)
 
 #
-xyc_list=np.zeros((6,predictOutput_np.shape[0],3), dtype = float, order = 'C')
-# xyc_list=np.zeros(6,1,1)
+
+
+xyc_list=np.zeros((6,predictOutput_np.shape[0],4), dtype = "float", order = 'C')
+
+
+
+
 pass
 
 #
@@ -28,41 +33,62 @@ N_row=0
 N_col=0
 for row in predictOutput_np:
     for count in row:
-        xyc_list[N_row][N_col][0]=N_col
-        xyc_list[N_row][N_col][1]=count
-        xyc_list[N_row][N_col][2]=0
-
-        N_col += 1
-    pass
-    N_col=0
-    N_row += 1
-pass
-
-
-#
-N_row=0
-N_col=0
-for row in gold_standard_np:
-    for count in row:
+        xyc_list[N_row][N_col][0]=N_row
+        xyc_list[N_row][N_col][1]=N_col
         xyc_list[N_row][N_col][2]=count
+        xyc_list[N_row][N_col][3]=0
 
         N_col += 1
     pass
     N_col=0
     N_row += 1
 pass
-#
-xyc_list_output_list= "xyc_list"
-# list2DWriteTo(xyc_list[0],xyc_list_0_path)
-# osOpenFile(xyc_list_0_path)
 
-write_3DnpTo2DCsv(xyc_list, xyc_list_output_list, 1)
+#
+# N_row=0
+# N_col=0
+# for row in gold_standard_np:
+#     for count in row:
+#
+#         if count:
+#             xyc_list[N_row][N_col][3]="r"
+#         else:
+#             xyc_list[N_row][N_col][3] = "g"
+#
+#         N_col += 1
+#     pass
+#     N_col=0
+#     N_row += 1
+# pass
+#
+xyc_list_output_list= "xyc_list.csv"
+list2DWriteTo(xyc_list[0],xyc_list_output_list)
+#
+# N_row=0
+# N_col=0
+# for row in predictOutput_np:
+#     for count in row:
+#
+#
+#     pass
+#     N_col=0
+#     N_row += 1
+# pass
+# xyc_list_output_list= "xyc_list"
+# # # list2DWriteTo(xyc_list[0],xyc_list_0_path)
+# # # osOpenFile(xyc_list_0_path)
+# #
+# write_3DnpTo2DCsv(xyc_list, xyc_list_output_list, 1)
 
 #
 
 # print(predictOutput_np)
 # print(gold_standard_np.shape)
 
-# plt.scatter(x, y, c=colors,alpha=0.3,s=40)
-# plt.show()
+# x=
+#
+# # plt.scatter(x, y, c=colors,alpha=0.3,s=40)
+# print(xyc_list[0,:,1])
+plt.scatter(xyc_list[0,:,0],xyc_list[0,:,2])
+plt.show()
 pass
