485ed82fe33bf5e116ffdb79a1c33beac382907f
LF-308 <hujingtian@126.com>
2024-01-22 04:16:04 +0800

    24-1-22:错在哪里？？？？？

--- my_predict_graphPlot/graphPlot.py
+++ my_predict_graphPlot/graphPlot.py
@@ -5,18 +5,16 @@ from myFunctionFolder.my_CSV_Function import getCsvReader, CsvTo2DList, list2DWr
 from myFunctionFolder.my_Numpy_Function import *
 
 #
-predictOutput_path="./predictOutput.csv"
-predictOutput_np=csvToNp(predictOutput_path)
+predictOutput_path = "./predictOutput.csv"
+predictOutput_np = csvToNp(predictOutput_path)
 
-gold_standard_path="gold_standard.csv"
-gold_standard_np=csvToNp(gold_standard_path)
+gold_standard_path = "gold_standard.csv"
+gold_standard_np = csvToNp(gold_standard_path)
 
 #
 
-
-xyc_list=np.zeros((6,predictOutput_np.shape[0],4), dtype = "float", order = 'C')
-
-
+xyc_list_dtype = np.dtype([('sort', 'int'), ('id', 'int'), ('pre', 'float'), ('color', 'str')])
+xyc_list=np.zeros((predictOutput_np.shape[0]*6), dtype =xyc_list_dtype, order = 'C')
 
 
 pass
@@ -25,70 +23,33 @@ pass
 '''
 至此，csv被转置为横版
 '''
-predictOutput_np=predictOutput_np.T
-gold_standard_np=gold_standard_np.T
+predictOutput_np = predictOutput_np.T
+gold_standard_np = gold_standard_np.T
 
 #
-N_row=0
-N_col=0
+N_row = 0
+N_col = 0
+persionID=0
 for row in predictOutput_np:
     for count in row:
-        xyc_list[N_row][N_col][0]=N_row
-        xyc_list[N_row][N_col][1]=N_col
-        xyc_list[N_row][N_col][2]=count
-        xyc_list[N_row][N_col][3]=0
+        color = ""
+        if gold_standard_np[N_row][N_col] == 1:
+            color = "r"
+        else:
+            color = "g"
+        print((N_row, N_col, count, color))
+        xyc_list[persionID]=(N_row, N_col, count, color)
 
         N_col += 1
+        persionID+=1
     pass
-    N_col=0
+    N_col = 0
     N_row += 1
 pass
 
+xyc_list_output_path= "xyc_list.csv"
+list2DWriteTo(xyc_list, xyc_list_output_path)
 #
-# N_row=0
-# N_col=0
-# for row in gold_standard_np:
-#     for count in row:
-#
-#         if count:
-#             xyc_list[N_row][N_col][3]="r"
-#         else:
-#             xyc_list[N_row][N_col][3] = "g"
-#
-#         N_col += 1
-#     pass
-#     N_col=0
-#     N_row += 1
-# pass
-#
-xyc_list_output_list= "xyc_list.csv"
-list2DWriteTo(xyc_list[0],xyc_list_output_list)
-#
-# N_row=0
-# N_col=0
-# for row in predictOutput_np:
-#     for count in row:
-#
-#
-#     pass
-#     N_col=0
-#     N_row += 1
-# pass
-# xyc_list_output_list= "xyc_list"
-# # # list2DWriteTo(xyc_list[0],xyc_list_0_path)
-# # # osOpenFile(xyc_list_0_path)
-# #
-# write_3DnpTo2DCsv(xyc_list, xyc_list_output_list, 1)
-
-#
-
-# print(predictOutput_np)
-# print(gold_standard_np.shape)
-
-# x=
-#
-# # plt.scatter(x, y, c=colors,alpha=0.3,s=40)
-# print(xyc_list[0,:,1])
-plt.scatter(xyc_list[0,:,0],xyc_list[0,:,2])
-plt.show()
+# plt.scatter(xyc_list[0, :, 0], xyc_list[0, :, 2])
+# plt.show()
 pass
